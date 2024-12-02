6c52df143099648caa643a2350733c743a187cd0
[Ivana Nedeljkov] <[ivkaned@gmail.com]>
2024-12-02 21:38:13 +0100

    Some more installments

--- app/page.js
+++ app/page.js
@@ -9,10 +9,10 @@ export default function Home() {
     <div
       className="relative min-h-screen bg-cover bg-center"
       style={{
-        backgroundImage: "url('/background.png')", // Set the background image
+        backgroundImage: "url('/background.png')", 
       }}
     >
-      <div className="absolute inset-0 bg-gradient-to-b from-black via-[#000974] to-[#000974] opacity-70"></div> {/* Gradient overlay */}
+      <div className="absolute inset-0 bg-gradient-to-b from-black via-[#000974] to-[#000974] opacity-70"></div> 
       <div className="relative z-10">
         <Navbar />
         <main className="pt-16">
